0a7208949dfbcfa3282eeb79f183f5c08d30f38b
Anton Sergeenkov <asergeenkov@combotech.net>
2025-12-21 23:07:26 +0400

    2

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,11 @@
-import FilmsList from '../FilmsList'
-import SearchFilms from '../SearchFilms'
+import { Outlet } from "react-router";
+import Header from '../../components/Header'
 
 const App = () => {
   return (
     <>
-      {/* <FilmsList /> */}
-      <SearchFilms />
+      <Header />
+      <Outlet />
     </>
   )
 }
