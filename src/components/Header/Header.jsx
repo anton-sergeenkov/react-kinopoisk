81c38d97b295f906a9e49e6cd46cd686d5501290
Anton Sergeenkov <asergeenkov@combotech.net>
2025-12-25 22:02:40 +0400

    Bash Script Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router";
+import cn from "classnames";
+import { NavLink } from "react-router";
 
 import styles from './styles.module.css'
 
 const Header = () => {
+  const setActiveClass = (statuses) => {
+    return statuses.isActive ? cn(styles.link, styles.linkActive) : styles.link;
+  };
+
   return (
     <div className={styles.wrapper}>
-      <Link to="/">Home</Link>
-      <Link to="/films">Список фильмов</Link>
-      <Link to="/search">Поиск фильма</Link>
+      <NavLink to="/" className={setActiveClass}>Home</NavLink>
+      <NavLink to="/films" className={setActiveClass}>Список фильмов</NavLink>
+      <NavLink to="/search" className={setActiveClass}>Поиск фильма</NavLink>
     </div>
   )
 }
